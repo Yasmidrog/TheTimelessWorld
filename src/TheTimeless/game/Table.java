041379b0698f9f881342311933511dbf4fcd9cda
yasmidrog <yasmidrog@gmail.com>
2015-01-19 17:30:29 +0300

    Set levels

--- src/TheTimeless/game/Table.java
+++ src/TheTimeless/game/Table.java
@@ -66,7 +66,6 @@ public class Table extends Entity {
         if (sprite != null)
             sprite.draw(-CrWld.SpMn.x + x + CrWld.CrCntr.getWidth() / 2-CrWld.SpMn.SzW /2,
                     -CrWld.SpMn.y + y + CrWld.CrCntr.getHeight() / 2-CrWld.SpMn.SzH /2);
-
     }
     public void drawTable() {
         if (showText) {
